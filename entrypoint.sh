6790f3ee65d77d5a25dc340cc681c2e49b332c7a
zhu5201314 <46183853+zhu5201314@users.noreply.github.com>
2020-05-21 15:57:06 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,14 @@ time=$(date)
 echo $INPUT_WHO_TO_GREET
 echo $INPUT_WP_DB_USER
 echo ::set-output name=time::$time
+inputstr="(1, 2, 3)"
+
+newstr=$(echo $INPUT_WHO_TO_GREET | sed 's/[[]]//g' ) # remove ( and )
+
+IFS=', ' read -r -a myarray <<< "$newstr"
+for index in "${!myarray[@]}"
+do
+    # echo "$index ${myarray[index]}"  #  shows index and value
+      echo        "${myarray[index]}"  #  shows           value
+done
 
