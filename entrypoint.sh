07c16ceab92c6e284aed0a6b910ad4a73366fe28
zhu5201314 <46183853+zhu5201314@users.noreply.github.com>
2020-05-21 16:50:18 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,5 +9,5 @@ echo ::set-output name=time::$time
 newstr=$(echo $INPUT_WHO_TO_GREET | sed 's/\[//g' ) # remove [ 
 newstr2=$(echo $newstr | sed 's/\]//g' ) # remove ]
 echo $newstr2
-IFS=',' read -r -a myarray <<< "$newstr2" # delimiter is ,
-echo myarray[0]
+IFS=',' read -r -a myarray <<<"$newstr2" # delimiter is ,
+echo ${array[@]}  
